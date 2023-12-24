52ef0f02d34f2d12688f1ab29c4fa2ab11570b4e
esslam-ashour <esslamhosam@gmail.com>
2023-12-23 23:31:01 -0600

    added links

--- app/page.js
+++ app/page.js
@@ -14,10 +14,10 @@ export default function Experience() {
     
       <main className=' bg-gradient-to-tl from-indigo-500 via-slate-900 to-black px-10 md:px-20 lg:px-40 max-w-screen'>
         <nav className="text-4xl md:text-5xl lg:text-5xl flex justify-center gap-10 py-3 flex-row md:flex flex-grow pt-10">
-            <Link href="https://linkedin.com/in/esslam-ashour"><AiOutlineLinkedin className="text-white hover:text-indigo-600 transition ease-in" /></Link>
-            <Link href="https://github.com/esslam-ashour"><AiOutlineGithub className="text-white hover:text-indigo-600 transition ease-in" /></Link>
-            <Link href="mailto:esslamhosam@gmail.com"><AiOutlineMail className="text-white hover:text-indigo-600 transition ease-in" /></Link>
-            <Link href="/resume.pdf"><CgFileDocument className="text-white hover:text-indigo-600 transition ease-in"/></Link>
+            <a rel="noopener noreferrer" target="_blank" href="https://linkedin.com/in/esslam-ashour"><AiOutlineLinkedin className="text-white hover:text-indigo-600 hover:translate-y-1 transition ease-in" /></a>
+            <a rel="noopener noreferrer" target="_blank" href="https://github.com/esslam-ashour"><AiOutlineGithub className="text-white hover:text-indigo-600 hover:translate-y-1 transition ease-in" /></a>
+            <a href="mailto:esslamhosam@gmail.com"><AiOutlineMail className="text-white hover:text-indigo-600 hover:translate-y-1 transition ease-in" /></a>
+            <Link rel="noopener noreferrer" target="_blank" href="/resume.pdf"><CgFileDocument className="text-white hover:text-indigo-600 hover:translate-y-1 transition ease-in"/></Link>
         </nav>
         <section className="min-h-screen">
           <div className="justify-center p-10">
@@ -38,7 +38,7 @@ export default function Experience() {
             <div className="border-2 border-indigo-500 bg-opacity-25 flex flex-col bg-white text-indigo-500 px-10 py-10 mr-8 mt-4 mb-4 rounded-xl">
               <h3 className="font-bold text-2xl md:text-3xl lg:text-4xl pb-3 text-center md:text-left lg:text-left text-white">Projects</h3>
               <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-3">
-                <Link href="#"><p className="border-2 border-white transition ease-in hover:bg-indigo-700 hover:translate-y-1 bg-indigo-500 text-white rounded-xl py-2 px-3 text-center">GoQuizCLI<br/><span className='text-sm text-gray-300'>A CLI quiz app</span></p></Link>
+                <a rel="noopener noreferrer" target="_blank" href="https://github.com/esslam-ashour/GoQuizCLI"><p className="border-2 border-white transition ease-in hover:bg-indigo-700 hover:translate-y-1 bg-indigo-500 text-white rounded-xl py-2 px-3 text-center">GoQuizCLI<br/><span className='text-sm text-gray-300'>A CLI quiz app</span></p></a>
                 <Link href="#"><p className="border-2 border-white transition ease-in hover:bg-indigo-700 hover:translate-y-1 bg-indigo-500 text-white rounded-xl py-2 px-3 text-center">MuhMoney <br/><span className='text-sm text-gray-300'>An expense tracker</span></p></Link>
               </div>
             </div>
