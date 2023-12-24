015977b184a890938244ca6cf3dc6b596870773b
esslam-ashour <esslamhosam@gmail.com>
2023-12-24 01:17:28 -0600

    minor style changes, removed one skill entry

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Experience() {
         <link rel="icon" href="/favicon.ico" sizes='any'/>
       </Head>
     
-      <main className="bg-gradient-to-tl from-zinc-800 via-slate-900 to-black md:px-10 lg:px-30 max-w-screen">
+      <main className="bg-gradient-to-tl from-zinc-500 via-slate-900 to-black md:px-10 lg:px-30 max-w-screen">
         <nav className="text-4xl md:text-5xl lg:text-5xl flex justify-center gap-10 py-3 flex-row md:flex flex-grow pt-10">
             <a rel="noopener noreferrer" target="_blank" href="https://linkedin.com/in/esslam-ashour"><AiOutlineLinkedin className="text-white hover:text-zinc-600 hover:translate-y-1 transition ease-in" /></a>
             <a rel="noopener noreferrer" target="_blank" href="https://github.com/esslam-ashour"><AiOutlineGithub className="text-white hover:text-zinc-600 hover:translate-y-1 transition ease-in" /></a>
@@ -36,27 +36,24 @@ export default function Experience() {
           <div>
           <div className="justify-end pl-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
             <div className="border-2 border-zinc-800 bg-opacity-10 flex flex-col bg-white text-zinc-800 px-10 py-10 mr-8 mt-4 mb-4 rounded-xl">
-              <h3 className="font-bold text-2xl md:text-3xl lg:text-4xl pb-3 text-center md:text-left lg:text-left text-white">Projects</h3>
-              <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-3">
-                <a rel="noopener noreferrer" target="_blank" href="https://github.com/esslam-ashour/GoQuizCLI"><p className="border-2 border-white transition ease-in hover:bg-zinc-700 hover:font-bold hover:translate-y-1 bg-zinc-800 text-white rounded-xl py-2 px-3 text-center">GoQuizCLI<br/><span className='text-sm text-gray-300'>A CLI quiz app</span></p></a>
-                <Link href="#"><p className="border-2 border-white transition ease-in hover:bg-zinc-700 hover:translate-y-1 bg-zinc-800 text-white rounded-xl py-2 px-3 text-center hover:font-bold">MuhMoney<br/><span className='text-sm text-gray-300'>An expense tracker</span></p></Link>
-                <Link href="https://www.esslam-ashour.com"><p className="border-2 border-white transition ease-in hover:bg-zinc-700 hover:translate-y-1 bg-zinc-800 text-white rounded-xl py-2 px-3 text-center hover:font-bold">This website!<br/><span className='text-sm text-gray-300'>Made using Next.js</span></p></Link>
+              <h3 className="font-bold text-2xl md:text-3xl lg:text-4xl pb-3 text-center text-white">Projects</h3>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-8">
+                <a rel="noopener noreferrer" target="_blank" href="https://github.com/esslam-ashour/GoQuizCLI"><p className="border-2 border-white transition ease-in hover:bg-zinc-700 hover:font-bold hover:translate-y-1 bg-zinc-800 text-white rounded-xl py-2 px-3 text-center">GoQuizCLI<br/><span className='text-sm text-gray-300'>A CLI quiz app made using Golang</span></p></a>
+                <Link href="#"><p className="border-2 border-white transition ease-in hover:bg-zinc-700 hover:translate-y-1 bg-zinc-800 text-white rounded-xl py-2 px-3 text-center hover:font-bold">MuhMoney<br/><span className='text-sm text-gray-300'>An expense tracker made using React</span></p></Link>
+                <Link href="https://www.esslam-ashour.com"><p className="border-2 border-white transition ease-in hover:bg-zinc-700 hover:translate-y-1 bg-zinc-800 text-white rounded-xl py-2 px-3 text-center hover:font-bold">This website!<br/><span className='text-sm text-gray-300'>Made using Next.js and Tailwind CSS</span></p></Link>
               </div>
             </div>
             <div className="border-2 border-white bg-opacity-10 flex flex-col bg-zinc-800 text-white px-10 py-10 mr-8 mt-4 mb-4 rounded-xl">
-              <h3 className="font-bold text-2xl md:text-3xl lg:text-4xl pb-3 text-center md:text-left lg:text-left">Skills</h3>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-3">
+              <h3 className="font-bold text-2xl md:text-3xl lg:text-4xl pb-3 text-center">Skills</h3>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-8">
                 <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Python</p>
                 <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Go</p>
                 <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">JavaScript</p>
                 <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">SQL</p>
                 <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Bash</p>
-                <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Next.js</p>
-                <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Django</p>
-                <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Tailwind</p>
+                <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Next.js</p>                <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Tailwind</p>
                 <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Linux</p>
                 <p className="border-2 border-zinc-800 bg-white text-zinc-800 rounded-xl py-2 px-3 text-center truncate">Git</p>
-                <p className="py-2 px-3">and more..</p>
               </div>
             </div>
           </div>
